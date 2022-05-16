8d052910090b30696ca923e440ce791e7634938c
steveen <ss.rodriguez00021@gmail.com>
2022-05-16 01:46:24 -0500

    Se agregan link de iconos y de letras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,39 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spotify';
-
-  // Tipos de Datos TypeScript
-  name:string = 'stevee'
-  edad:number = 25
-  phone: null= null
-  phone1: undefined = undefined
-  datum: any = 12
-
-  status: string|number = 'fail'
-
-  car: CarModel = {
-    brand: 'Ford',
-    model: 'Focus',
-    year: 2021
-  }
-
-  listCars: Array<CarModel> = [
-    {
-      brand:'crevrolet',
-      model: 'Camaro',
-      year: 2021
-    },
-    {
-      brand:'BMW',
-      model: 'Corsa',
-      year: 2022
-    }
-  ] 
-  
 }
 
-interface CarModel {
-  brand: string;
-  model: string;
-  year?: number;
-}
