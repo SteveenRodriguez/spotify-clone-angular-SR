89e22c8b6168926109da57904de92c9999678074
steveen <ss.rodriguez00021@gmail.com>
2022-05-18 22:24:59 -0500

    creación de pages en los modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FavoritesPageComponent } from './modules/favorites/pages/favorites-page/favorites-page.component';
 
 @NgModule({
   declarations: [ //TODO: pueden ser, Declaraciones, componentes, directivas y pipes
-    AppComponent
+    AppComponent, FavoritesPageComponent
   ],
   imports: [ //TODO: solo se importan otros modules
     BrowserModule,
